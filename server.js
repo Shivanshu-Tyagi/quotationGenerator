684d3b65b1f84e53a43c39c1a1677467a6c38916
Shivanshu-Tyagi <shivanshu.tyagi@zcrewindia.com>
2026-03-06 12:33:54 +0530

    Fix CORS ..

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ cloudinary.config({
 });
 
 // ── Middleware ────────────────────────────────────────────────────────────
-app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
